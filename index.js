c2ff10a9833b97e92cb29c2da06b52c4f16789cb
Markell Owens <markellowens@mac.com>
2018-08-22 19:44:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -59,5 +59,13 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+   if (Number.isInteger(cardNumber) ){
+    var valtotal = total();
+    cart.length = 0;
+    return `Your total cost is $${valtotal}, which will be charged to the card ${cardNumber}.`;
+  }
+  else{
+    return `Sorry, we don't have a credit card on file for you.`;
+  }
+  return;
 }
