f567d5bf62dc67b7a3ef830f7de94a4e5eaecd48
Markell Owens <markellowens@mac.com>
2018-08-22 19:42:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,17 @@ var obj = {};
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length === 0 ){
+    return 'Your shopping cart is empty.';
+  }
+  else if (cart.length === 1){
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
+  }
+  var sentence = `In your cart, you have`;
+  for (var i =0; i < cart.length - 1 ; i++){
+    sentence = sentence + ` ${cart[i].itemName} at $${cart[i].itemPrice},`;
+  }
+  return `${sentence} and ${cart[cart.length - 1].itemName} at $${cart[cart.length - 1].itemPrice}.`;
 }
 
 function total() {
